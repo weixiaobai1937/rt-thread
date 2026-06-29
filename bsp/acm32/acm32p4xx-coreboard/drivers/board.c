8b554ada49d230a74d91a92e0434f523bfca3ab8
RT-Thread Port <port@acm32p4xx.local>
2026-06-29 14:53:19 +0800

    [acm32][coreboard] Add drivers config, board init, and GCC startup file

---
+++ bsp/acm32/acm32p4xx-coreboard/drivers/board.c
@@ -0,0 +1,41 @@
+#include <rthw.h>
+#include <rtthread.h>
+#include <rtdevice.h>
+#include "board.h"
+
+#define SOC_SRAM_END_ADDR   (SOC_SRAM_START_ADDR + SOC_SRAM_SIZE * 1024)
+
+extern int __bss_end;
+extern void rt_hw_uart_init(void);
+
+void SysTick_Handler(void)
+{
+    rt_interrupt_enter();
+    rt_tick_increase();
+    rt_interrupt_leave();
+}
+
+void rt_hw_board_init(void)
+{
+    /* 1. ACM32P4 SDK system init (clock/NVIC/TIM64B/PSRAM) */
+    system_init();
+
+    /* 2. Configure SysTick */
+    sys_info_t info;
+    system_get_info(&info);
+    SysTick_Config(info.sysclk_hz / RT_TICK_PER_SECOND);
+
+    /* 3. UART + console */
+    rt_hw_uart_init();
+    rt_console_set_device(RT_CONSOLE_DEVICE_NAME);
+
+    /* 4. Heap memory */
+#ifdef RT_USING_HEAP
+    rt_system_heap_init((void *)&__bss_end, (void *)SOC_SRAM_END_ADDR);
+#endif
+
+    /* 5. Component init */
+#ifdef RT_USING_COMPONENTS_INIT
+    rt_components_board_init();
+#endif
+}
---
+++ bsp/acm32/acm32p4xx-coreboard/drivers/board.h
@@ -0,0 +1,23 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+#include <rtconfig.h>
+#include "acm32p4.h"
+
+/* UART1: TX=PA9(AF1), RX=PA10(AF1) - console */
+#if defined(BSP_USING_UART1)
+    #define UART1_TX_PIN            PA9
+    #define UART1_RX_PIN            PA10
+    #define UART1_AF                GPIO_AF_1
+#endif
+
+/* Coreboard LED: PA0 */
+#define LED_PIN                  PA0
+
+/* SOC memory config */
+#define SOC_SRAM_START_ADDR      (0x20000000)
+#define SOC_SRAM_SIZE            (128)   /* KB */
+
+void rt_hw_board_init(void);
+
+#endif /* __BOARD_H__ */
