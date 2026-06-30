3b7aeaaba717a46fa3438bc98cbc26d8ec4149f3
RT-Thread Port <port@acm32p4xx.local>
2026-06-30 13:18:19 +0800

    [acm32][drivers] config: add ETH Kconfig and pin definitions

--- bsp/acm32/acm32p4xx-coreboard/drivers/board.h
+++ bsp/acm32/acm32p4xx-coreboard/drivers/board.h
@@ -11,6 +11,29 @@
     #define UART1_AF                GPIO_AF_1
 #endif
 
+/* ETH RMII: LAN8720A PHY, all AF6 */
+#if defined(BSP_USING_ETH)
+    #define ETH_RST_PIN             PC0
+    #define ETH_REF_CLK_PIN         PA1
+    #define ETH_REF_CLK_AF          GPIO_AF_6
+    #define ETH_MDIO_PIN            PA2
+    #define ETH_MDIO_AF             GPIO_AF_6
+    #define ETH_CRS_DV_PIN          PA7
+    #define ETH_CRS_DV_AF           GPIO_AF_6
+    #define ETH_MDC_PIN             PC1
+    #define ETH_MDC_AF              GPIO_AF_6
+    #define ETH_RXD0_PIN            PC4
+    #define ETH_RXD0_AF             GPIO_AF_6
+    #define ETH_RXD1_PIN            PC5
+    #define ETH_RXD1_AF             GPIO_AF_6
+    #define ETH_TX_EN_PIN           PB11
+    #define ETH_TX_EN_AF            GPIO_AF_6
+    #define ETH_TXD0_PIN            PB12
+    #define ETH_TXD0_AF             GPIO_AF_6
+    #define ETH_TXD1_PIN            PB13
+    #define ETH_TXD1_AF             GPIO_AF_6
+#endif
+
 /* Coreboard LED: PA0 */
 #define LED_PIN                  PA0
 
